3db38685bdc68dcf964d94a80c7f79305f69317e
noboru-i <ishikura.noboru@gmail.com>
2015-09-03 15:54:16 +0900

    remove TODO

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'ios_analytics'
+require 'coveralls'
+Coveralls.wear!
+
+require 'bundler'
+Bundler.require :default, :development
